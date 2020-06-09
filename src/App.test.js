8a51128df5522c0c757b97d0ddcbc9467274161d
Julie <j49411@gmail.com>
2020-06-09 15:42:33 +0100

    test: button and picture components are rendered as child components

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,8 @@ import '@testing-library/jest-dom/extend-expect'
 
 import App from './App'
 import Header from './components/Header/Header'
+import Button from './components/Button/Button'
+import Picture from './components/Picture/Picture'
 
 
 describe('App', () => {
@@ -16,6 +18,14 @@ describe('App', () => {
 
   it('should render the Header Component', () => {
     expect(wrapper.containsMatchingElement(<Header />)).toEqual(true)
+  })
+
+  it('should render the Button Component', () => {
+    expect(wrapper.containsMatchingElement(<Button />)).toEqual(true)
+  })
+
+  it('should render the Picture Component', () => {
+    expect(wrapper.containsMatchingElement(<Picture />)).toEqual(true)
   })
 
  
