b103008c8a678220477d55135cedd5d73cb8e469
Julie <j49411@gmail.com>
2020-08-05 15:24:56 +0100

    test: app renders correctly

--- counter/src/App.test.js
+++ counter/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+
+test('renders without error', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
+})
+
+
